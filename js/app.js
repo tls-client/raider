b0e8c175e685b8d5998afec27cbd90e2cf4d39fa
Rucy <127585375+tls-client@users.noreply.github.com>
2026-03-04 07:08:55 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,43 @@
-import { readConfig } from "./ui.js";
-import { startEngine, stopEngine } from "./engine.js";
+import {state} from "./state.js";
+import {start,stop} from "./engine.js";
+import {log} from "./logger.js";
 
-document
-  .getElementById("startBtn")
-  .onclick = () => {
-    readConfig();
-    startEngine();
-  };
+function readConfig(){
 
-document
-  .getElementById("stopBtn")
-  .onclick = stopEngine;
+ state.config={
+  token:token.value,
+  guild:guild.value,
+  channels:channels.value,
+  message:message.value,
+  delay:Number(delay.value),
+  limit:Number(limit.value)
+ };
+
+}
+
+start.onclick=()=>{
+ readConfig();
+ start();
+};
+
+stop.onclick=stop;
+
+join.onclick=()=>{
+ log("サーバー参加処理");
+};
+
+leave.onclick=()=>{
+ log("サーバー退出処理");
+};
+
+fetchChannels.onclick=()=>{
+ log("チャンネル取得");
+};
+
+fetchUsers.onclick=()=>{
+ log("ユーザー取得");
+};
+
+endPoll.onclick=()=>{
+ log("投票終了");
+};
